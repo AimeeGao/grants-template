9d124689193629d7475ba0d68097d69a3892438b
AimeeGao <Aimee.Gao@gov.bc.ca>
2025-12-03 11:31:53 -0800

    Update Student module routes

--- Modules/Student/routes/web.php
+++ Modules/Student/routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Student\Http\Controllers\StudentController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('students', StudentController::class)->names('student');
+
+Route::prefix('student')->middleware(['auth', 'verified'])->group(function () {
+    Route::group(['as' => 'student.'], function () {
+        Route::get('/dashboard', [StudentController::class, 'index'])
+            ->name('dashboard');
+        Route::prefix('profile')->group(function () {
+            Route::get('/', [StudentController::class, 'profile'])->name('profile.index');
+            Route::put('/', [StudentController::class, 'update'])->name('profile.update');
+            Route::delete('/', [StudentController::class, 'destroy'])->name('profile.destroy');
+        });
+    });
+
 });
